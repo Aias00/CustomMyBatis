75a4377b54be9180d701d60928075f7b455d8757
aias00 <rokkki@163.com>
2019-09-03 22:01:02 +0800

    add cache function

--- src/main/java/com/aias/demo/mybatis/executor/CachingExecutor.java
+++ src/main/java/com/aias/demo/mybatis/executor/CachingExecutor.java
@@ -4,19 +4,30 @@ package com.aias.demo.mybatis.executor;
 import com.aias.demo.mybatis.config.Configuration;
 import com.aias.demo.mybatis.registry.MapperData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CachingExecutor implements Executor {
 
     private final Executor delegate;
     private Configuration configuration;
 
+    private Map<String, Object> localCache = new HashMap<>();
+
     public CachingExecutor(Executor executor) {
         this.delegate = executor;
         this.configuration = executor.getConfiuration();
     }
+
     @Override
     public <T> T execute(MapperData mapperData, Object parameter) throws Exception {
-        // TODO 调用缓存
-        return delegate.execute(mapperData, parameter);
+        // 调用缓存
+        Object result = localCache.get(mapperData.getSql());
+        if (null == result) {
+            result = delegate.execute(mapperData, parameter);
+            localCache.put(mapperData.getSql(), result);
+        }
+        return (T) result;
     }
 
     @Override
